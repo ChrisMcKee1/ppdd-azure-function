4ef3d114a534f8b255ca92ab79d8cd0f3e3a3504
Chris McKee <chrismckee@microsoft.com>
2022-07-18 10:09:47 -0500

    Added Final Swagger for Easy Import

--- Shared.cs
+++ Shared.cs
@@ -24,24 +24,19 @@ namespace PublicAPIs
         {
             string url = $"https://api.publicapis.org/{endpointType}?";
 
-            url = model.title is null ? url : url + "title=" + model.title.Trim().ToLower() + "&";
-            url = model.description is null ? url : url + "description=" + model.description.Trim().ToLower() + "&";
-            url = model.auth is null ? url : url + "auth=" + model.auth.Trim().ToLower() + "&";
-            url = model.https is null ? url : url + "https=" + model.https.Trim().ToLower() + "&";
-            url = model.cors is null ? url : url + "cors=" + model.cors.Trim().ToLower() + "&";
-            url = model.category is null ? url : url + "category=" + model.category.Trim().ToLower() + "&";
+            url = model.title is null ? url : $"{url}title={model.title.Trim().ToLower()}&";
+            url = model.description is null ? url : $"{url}description={model.description.Trim().ToLower()}&";
+            url = model.auth is null ? url : $"{url}auth={model.auth.Trim().ToLower()}&";
+            url = model.https is null ? url : $"{url}https={model.https.Trim().ToLower()}&";
+            url = model.cors is null ? url : $"{url}cors={model.cors.Trim().ToLower()}&";
+            url = model.category is null ? url : $"{url}category={model.category.Trim().ToLower()}&";
             url = url[..^1];
 
-            HttpResponseMessage response = await _client.GetAsync(url);
-            string result = await response.Content.ReadAsStringAsync();
-            return new HttpResponseMessage(HttpStatusCode.OK)
-            {
-                Content = new StringContent(result, Encoding.UTF8, "application/json")
-            };
+            return await SendRequest(url);
         }
         public async Task<HttpResponseMessage> SendRequest(string endpointType)
         {
-            string url = $"https://api.publicapis.org/{endpointType}";
+            string url = endpointType.StartsWith("https://api.publicapis.org/") ? endpointType : $"https://api.publicapis.org/{endpointType}";
 
             HttpResponseMessage response = await _client.GetAsync(url);
             string result = await response.Content.ReadAsStringAsync();
